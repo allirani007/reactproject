7e9e80a82eb3ad8ec86904db4b2cc86212387fb4
allirani007 <alliranisss@gmail.com>
2022-02-26 07:23:04 +0530

    receipe

--- recipe_app/src/App.js
+++ recipe_app/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import{Link,Switch,Route} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <ul>
+      <li><Link to="/recipe-list">RecipeList</Link></li>
+      <li><Link to="/">Home</Link></li>
+      </ul>
+      <Switch>
+        <Route path="/recipe-list"><RecipeList /></Route>
+        <Route path="/"><Welcome/> </Route>
+      </Switch>
+   
+    
     </div>
   );
+}
+function Welcome(){
+  const msg="Welcome to recipe app 🍩🎂🍨";
+  return(
+    <div>
+      <h1>{msg}</h1>
+    </div>
+  );
+}
+
+function RecipeList(){
+//   const listrecipe=[{
+//     name:" Veg Biryani",
+//     pic:"https://i.ytimg.com/vi/JqP4wSWsYr4/maxresdefault.jpg"
+//   },
+//   {
+//     name:" Butter Naan",
+//     pic:"https://simmertoslimmer.com/wp-content/uploads/2020/02/Naan-Bread.jpg",
+//   },
+//   {
+//     name:"Veg curry",
+//     pic:"https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/vegetablecurry_80763_16x9.jpg",
+//   },
+//   {
+//     name: "gopi 65",
+//     pic:"https://www.boldsky.com/img/2021/07/img-20210728-wa0016-1627476057.jpg",
+//   },
+// ]
+const[listrecipe,setlistrecipe]=useState([]);
+useEffect(()=>{fetch("https://60f821399cdca0001745522d.mockapi.io/Recipe")
+.then(data=>data.json())
+.then(recipes=>setlistrecipe(recipes))
+},[]);
+  const msg="Awesome to recipe List🍗🥠";
+  return(
+    <div>
+      <h1>{msg}</h1>
+      <div className="recipe-list-Container">
+       {listrecipe.map(rrr=> <Recipe recipe={rrr}/>)} 
+      </div>
+      </div>
+  );
+}
+function Recipe({recipe}){
+    return(
+    <div className="recipe-container">
+      <img src={recipe.pic} alt={recipe.name} className="recipe_picture" />
+      <p className="recipe-name">{recipe.name}</p>
+    </div>
+  )
+
 }
 
 export default App;
